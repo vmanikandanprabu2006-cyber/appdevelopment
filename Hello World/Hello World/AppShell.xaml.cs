cfe9e1739d27f61f2da95ced693b37adcc17f7c6
vmani <vmani@15-FD0746TU>
2026-04-08 11:38:59 +0530

    Created HelloWorld project

---
+++ Hello World/Hello World/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Hello_World
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Hello World/Hello World/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Hello_World
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
